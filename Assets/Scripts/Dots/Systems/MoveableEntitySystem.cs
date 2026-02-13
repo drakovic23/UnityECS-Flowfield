497b287649cabea2bc8264230693cb48ff4f7b55
Deni Rakovic <deni.rakovic23@gmail.com>
2026-02-13 17:49:24 -0500

    Add spatial map job

--- Assets/Scripts/Dots/Systems/MoveableEntitySystem.cs
+++ Assets/Scripts/Dots/Systems/MoveableEntitySystem.cs
@@ -1,11 +1,17 @@
 ﻿
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
 public partial struct MoveableEntitySystem : ISystem
 {
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<SpatialMap>();
+    }
     public void OnUpdate(ref SystemState state)
     {
         int width = 0;
@@ -25,13 +31,17 @@ public partial struct MoveableEntitySystem : ISystem
             return;
         }
 
-        foreach (var (moveableEntity, transform, velocity) 
-                 in SystemAPI.Query<RefRO<MoveableEntity>, RefRW<LocalTransform>, RefRW<PhysicsVelocity>>())
+        var spatialMap = SystemAPI.GetSingleton<SpatialMap>();
+        foreach (var (moveableEntity, transform, velocity, entity) 
+                 in SystemAPI.Query<RefRO<MoveableEntity>, RefRW<LocalTransform>, RefRW<PhysicsVelocity>>().WithEntityAccess())
         {
             float3 pos = transform.ValueRO.Position;
             // float3 playerPos = new float3()
             float distance = math.lengthsq(pos - playerTag.Position);
-
+            
+            
+            
+            // Movement logic
             if (distance < 1f)
             {
                 Debug.LogWarning("Entity reached target");
@@ -59,11 +69,111 @@ public partial struct MoveableEntitySystem : ISystem
             
         }
     }
+
+    public partial struct PopulateSpatialMapUpdateJob : IJobEntity
+    {
+        public NativeParallelMultiHashMap<int, Entity>.ParallelWriter MapWriter;
+        public int Width;
+        public int Height;
+        public int offsetX;
+        public int offsetY;
+        public void Execute(Entity entity, in LocalTransform transform)
+        {
+            int posX = (int)math.round(transform.Position.x) + offsetX;
+            int posY = (int)math.round(transform.Position.z) + offsetY;
+            if (posX < 0 || posX > Width - 1 || posY < 0 || posY > Height - 1)
+                return;
+            
+            int cellindex = posY * Width + posX;
+            
+            MapWriter.Add(cellindex, entity);
+        }
+    }
+    
+    public partial struct ReadSpatialMapUpdateJob : IJobEntity
+    {
+        public NativeParallelMultiHashMap<int, Entity> SpatialMap;
+        [ReadOnly] public ComponentLookup<LocalTransform> TransformLookup;
+        public float SeparationRadius;
+        public float SeparationWeight;
+        public int Width;
+        public int Height;
+        public int OffsetX;
+        public int OffsetY;
+        public void Execute(Entity entity, ref PhysicsVelocity velocity, in LocalTransform transform)
+        {
+            int posX = (int)math.round(transform.Position.x) + OffsetX;
+            int posY = (int)math.round(transform.Position.z) + OffsetY;
+            if (posX < 0 || posX > Width - 1 || posY < 0 || posY > Height - 1)
+                return;
+            
+            float3 separationForce = float3.zero;
+            
+            // Check 3 x 3 neighbors
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    int checkX = posX + x;
+                    int checkY = posY + y;
+                    
+                    if (checkX < 0 || checkX >= Width || checkY < 0 || checkY >= Height)
+                        continue;
+
+                    int neighborIndex = checkY * Width + checkX;
+                    
+                    NativeParallelMultiHashMapIterator<int> iterator;
+                    Entity neighbor;
+                    
+                    if (SpatialMap.TryGetFirstValue(neighborIndex, out neighbor, out iterator))
+                    {
+                        do
+                        {
+                            // Skip ourselves
+                            if (neighbor == entity)
+                                continue;
+                            
+                            float3 neighborPos = TransformLookup[neighbor].Position;
+                            float distance = math.lengthsq(transform.Position - neighborPos);
+                            
+                            if (distance < math.square(SeparationRadius)) // Square the radius so we have equal comparison
+                            {
+                                // flatten our current and neighbor positions
+                                float3 horizontalPosCurrent = new float3(transform.Position.x, 0, transform.Position.z);
+                                float3 horizontalPosNeighbor = new float3(neighborPos.x, 0, neighborPos.z);
+                                separationForce += math.normalizesafe(horizontalPosCurrent - horizontalPosNeighbor);
+                            }
+                        } while (SpatialMap.TryGetNextValue(out neighbor, ref iterator));
+                    }
+                }
+            }
+            
+            // Apply the force
+            float MaxSpeed = 5f;
+            if (!math.all(separationForce == 0))
+            {
+                velocity.Linear += separationForce * SeparationWeight;
+
+                float2 horizontalVelocity = new float2(velocity.Linear.x, velocity.Linear.z);
+                if(math.lengthsq(horizontalVelocity) > math.square(MaxSpeed))
+                {
+                    float3 horizontalVelocityScaled = math.normalizesafe(new float3(horizontalVelocity.x, 0, horizontalVelocity.y)) * MaxSpeed;
+                    float3 newVelocity = new float3(horizontalVelocityScaled.x, velocity.Linear.y, horizontalVelocityScaled.z);
+                    
+                    velocity.Linear = newVelocity;
+                }
+            }
+        }
+    }
 }
 
-// --------- Notes ---------
-// Currently, the entity just moves towards the target
-// When using hordes there is typically 3 parts to accomodate large hordes
-// (1) Separation - If a neighbor (ex: left neighbor) is too close to the entity, we need to apply a vector in the opposite direction (right)
-// (2) Alignment - Since the entity is assumed to move in "crowds", it would be optimal to steer towards an average vector of the entire crowd
-// (3) Cohesion - If the entity drifts away from the crowd, we need to move towards the average position of the crowd (center of mass)
+/*  --------------------Notes---------------------
+    Currently, the entity just moves towards the target
+    When using hordes there is typically 3 parts to accomodate large hordes
+    (1) Separation - If a neighbor (ex: left neighbor) is too close to the entity, we need to apply a vector in the opposite direction (right)
+    (2) Alignment - Since the entity is assumed to move in "crowds", it would be optimal to steer towards an average vector of the entire crowd
+    (3) Cohesion - If the entity drifts away from the crowd, we need to move towards the average position of the crowd (center of mass)
+    The flow field acts as alignment (entities are pulled together toward the target) and as alignment (the entities are forced to face the same direction)
+    As a result we can use flow field + separation to achieve similar results since the separation and alignment are redundant math
+    ----------------------------------------------
+*/
